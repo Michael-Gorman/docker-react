61d7871b6f2c584c40a83ba9579e311a70bf2ebd
Michael Gorman <michaelgr700@hotmail.co.uk>
2023-07-14 16:57:46 +0100

    Adding . to context andupdating changing app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I want this to show please!, edit <code>src/App.js</code> and save to reload.
+          Ok Now this should work!, edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
