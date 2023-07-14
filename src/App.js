257e052ffaeef691b69e677c187c3715ffdbbc87
Michael Gorman <michaelgr700@hotmail.co.uk>
2023-07-14 16:22:16 +0100

    updating changing app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there, edit <code>src/App.js</code> and save to reload.
+          I want this to show please!, edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
